36de3d46573eb9959f3f36f4dcd8345b05343c1f
涂滔 <851542486@qq.com>
2022-04-12 16:04:12 +0800

    apply

---
+++ apply.js
@@ -0,0 +1,18 @@
+/* 
+改变函数的this指向，第一个参数为this的指向，第二个参数为 函数执行的参数
+1.第一个参数为空的情况，this指向window
+2.第二个参数为null或者数组。
+*/
+
+Function.prototype._apply = function (context) {
+    if (!context) {
+        context = window;
+    }
+    let fn = Symbol(context);
+    context[fn] = this;
+    let arg = Array.prototype.slice.call(arguments, 1);
+    context[fn](arg);
+    delete context[fn];
+};
+
+
